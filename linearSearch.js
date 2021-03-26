59260e9a0b2d61fc72c5f52fbf12ae7aab2999ba
Dat Hoang <dthoang6@gmail.com>
2021-03-25 19:45:51 -0700

    implement linear search

---
+++ linearSearch.js
@@ -0,0 +1,8 @@
+function linearSearch(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return i;
+        }
+    }
+    return -1;
+}
