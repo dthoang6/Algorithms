e4f8903809cc28efad0716c220efa04b67d920e5
Dat Hoang <dthoang6@gmail.com>
2021-04-19 22:17:25 -0700

    using slice instead of splice to avoid side effects

--- trackTab.js
+++ trackTab.js
@@ -1,3 +1,5 @@
+// goal: using simple oop to track the tabs you have opened
+
 // tabs is an array of titles of each site open within the window
 var Window = function(tabs) {
     this.tabs = tabs; // We keep a record of the array inside the object
@@ -18,14 +20,13 @@ var Window = function(tabs) {
   // When you close a tab
   Window.prototype.tabClose = function (index) {
   
-    // Only change code below this line
   
-    var tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
-    var tabsAfterIndex = this.tabs.splice(index + 1); // Get the tabs after the tab
+  
+    var tabsBeforeIndex = this.tabs.slice(0, index); // Get the tabs before the tab
+    var tabsAfterIndex = this.tabs.slice(index + 1); // Get the tabs after the tab
   
     this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
   
-    // Only change code above this line
   
     return this;
    };
@@ -35,7 +36,7 @@ var Window = function(tabs) {
   var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
   var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
   
-  // Now perform the tab opening, closing, and other operations
+  // perform the tab opening, closing, and other operations
   var finalTabs = socialWindow
     .tabOpen() // Open a new tab for cat memes
     .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
