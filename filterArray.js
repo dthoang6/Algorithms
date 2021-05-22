1c7198be49723db1cbbc8b334bdf363d68287b40
Dat Hoang <dthoang6@gmail.com>
2021-05-21 17:24:10 -0700

    using two steps to reduce the amount of data to work with and return the items with desired format.

--- filterArray.js
+++ filterArray.js
@@ -112,7 +112,18 @@ var watchList = [
     }
   ];
   
-  var filteredList;
+  var filteredList = watchList
+  .map(movie => {
+    return {
+      title: movie.Title,
+      rating: movie.imdbRating
+    };
+  })
+  .filter(movie => {
+    // return true it will keep the item
+    // return false it will reject the item
+    return parseFloat(movie.rating) >= 8.0;
+  });
   
   
   console.log(filteredList);
