c40ba5af97db5a0a714c9ad812845b32a8fb0653
Dat Hoang <dthoang6@gmail.com>
2022-08-05 17:10:29 -0700

    learn the steps to solve coding interview problem

---
+++ algorithms/stepToSolveCodingInterview.js
@@ -0,0 +1,74 @@
+/* S1:Problem Details, key points 
+
+*/
+
+/* S2: Ask questions to confirm, double check input, output 
+
+*/
+
+/* S3: what is the most important value, main goal of the problem? time, space, readable 
+
+*/
+
+/* S4: Gather enough assumption to start solve problem 
+
+*/
+
+/* S5: Easy, brute force solution: first solution 
+
+*/
+
+/* S6: Explain why brute force solution is not optimal based on BigO
+
+*/
+
+/* S7: Try to improve the brute force solution: bottle neck, repeat code,
+common patterns 
+
+*/
+
+/* S8: Explain your new approach, walk through your code, steps you are going to
+follow. 
+
+*/
+
+/* S9: Modularize your code from the very beginning 
+
+*/
+
+/* S10: Start coding: 
+-write something, and start with the easy parts 
+-a lot of interviewers ask questions that your won't be able to fully answer on time.
+-how can I show that I can think, and working toward a solution.
+-break things up in function, makeup a function
+-------
+
+*/
+
+/* S11:Error check by breaking the code
+-never make assumption about input
+-always check for false inputs that you don't want
+-comment in your code, the checks that you want to do
+-tell the interviewer that you would write tests now to make function fail
+----
+
+*/
+
+/* S12: Test your code
+-check for no parameters, 0, undefined, null, massive inputs, async code...
+-ask the interviewer if we can make assumption about the code.
+-Can I make the answer return an error?
+-Am I repeating chunk of code ---> Functions
+----
+
+*/
+
+/* S13: Talk to interview where you would improve the code
+-Does it work?
+-Are there different approach?
+-Is it readable?
+-What would you google to improve?
+-How can performance be improved
+-What was the most interesting solution?
+----
+*/
